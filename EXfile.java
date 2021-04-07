626d8bcf66e00c165c2f6908e96c51d488a5318c
Troy Van Tiel <troyvantiel@gmail.com>
2021-04-07 15:03:46 +1200

    Added a read pdb method, added code to read and sort pdb file

--- EXfile.java
+++ EXfile.java
@@ -8,6 +8,64 @@ import java.io.IOException;
 
 public class EXfile
 {
+
+
+  public static void readpdb(String filename)
+  {
+  	try
+  	{
+  		boolean loop = true;
+  		int count = 0;
+  		int p = 0;
+  		String line = "";
+  		String split = " ";
+  		ArrayList<String> atomdata = new ArrayList<String>();
+  		ArrayList<List<String>> linedata = new ArrayList<List<String>>();
+    		String[] pdbline = null;
+  		BufferedReader pdbRead = new BufferedReader(new FileReader(filename));
+  		line = pdbRead.readLine();
+  		while(loop)
+  		{
+  			atomdata.clear();
+  			line = pdbRead.readLine();
+  			p = 0;
+  			System.out.println(line);
+  			pdbline = line.split(split);
+  			for(int i = 0; i < pdbline.length; i++)
+  			{
+				atomdata.add(pdbline[i]);
+				//System.out.println(i + "    " + data.get(i-p));
+				if(atomdata.get(i-p).isEmpty())
+				{
+					atomdata.remove(i-p);
+					p++; 
+
+				}
+  			}
+  			linedata.add(atomdata);
+  			count++;
+  			if(count > 2)
+  			{
+  				loop = false;
+  			}
+  		}
+  		for(int j = 0; j < 3; j++)
+  		{
+  			for(int t = 0; t < 12; t++)
+  			{
+  				System.out.println(linedata.get(j).get(t));
+  				//System
+  			}
+  		}
+  	}
+  	catch(IOException e)
+  	{
+  		e.printStackTrace();
+  	}
+  	
+  	
+  }
+
   public static void main(String[] args)
   {
 	try
@@ -21,17 +79,34 @@ public class EXfile
 		int atom4 = 0;
 		int k = 0;
 		int ref = 0;
+		boolean stop = false;
 		String file = args[0];
 		String dihed = "dihedral";
 		String splitBY = ",";
+		String line = "";
+		String[] data = null;
+		
 		
 		BufferedReader br = new BufferedReader(new FileReader(file));
-		
+		readpdb(file);
+		while(stop)
+		{
+			line = br.readLine();
+			data = line.split(splitBY);
+			
+			atom1 = Integer.parseInt(data[0]);
+			atom2 = Integer.parseInt(data[1]);
+			atom3 = Integer.parseInt(data[2]);
+			atom4 = Integer.parseInt(data[3]);
+			k = Integer.parseInt(data[4]);
+			ref = Integer.parseInt(data[5]);
+			
+		}
 		
 	}
-	catch
+	catch(IOException e)
 	{
-	
+		e.printStackTrace();
 	}	
   }
 
