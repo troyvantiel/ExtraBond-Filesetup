8676250985e8f5f056aa13b4d143af82568a7dac
Troy Van Tiel <troyvantiel@gmail.com>
2021-04-09 14:37:24 +1200

    added the code for reading the pdb and getting the lines that contain backbone atoms

--- EXfile.java
+++ EXfile.java
@@ -6,6 +6,91 @@ import java.io.IOException;
 
 
 public class EXfile {
+	public static void outputToFile(ArrayList<String[]> linedata)
+	{
+
+	}
+	public static void	Readpdb(String filename)
+	{
+		try
+		{
+			boolean loop = true;
+			boolean atomcheck = false;
+			int p = 0;
+			String line = "";
+			String split = " ";
+			List<String> bbatoms = Arrays.asList(new String[]{"C", "O", "N", "CA"});
+			ArrayList<String> atomdata = new ArrayList<String>();
+			ArrayList<String[]> linedata = new ArrayList<String[]>();
+			String[] pdbline = null;
+
+			BufferedReader pdbRead = new BufferedReader(new FileReader(filename));
+			line = pdbRead.readLine();
+			while(loop)
+			{
+				//clears the data to make sure nothing is copied from the last iteration
+				atomdata.clear();
+				//stores the next line of the file in a variable
+				line = pdbRead.readLine();
+				//resets the variable p to 0. p is the counter of how many data points have been removed from the line to keep the indexing consistent
+				p = 0;
+				//reset the atom check to false so that it can be set back to true if a backbone atom is found
+				atomcheck = false;
+				//split the line from the file by the delimiter (space in this case)
+				pdbline = line.split(split);
+				//for loop that will go through all the data points from each line that have been stored in the string array
+				for(int i = 0; i < pdbline.length; i++)
+				{
+					//add all the data to an arraylist to allow manipulation
+					atomdata.add(pdbline[i]);
+					//if statement to check the line if it contains nothing
+					if(atomdata.get(i-p).isEmpty())
+					{
+						//if the space is empty it can be removed from the array list of data which will compress and take away the spaces
+						atomdata.remove(i-p);
+						//increment p as there has been another data point removed (avoids the out of bounds errors!!)
+						p++;
+					}
+				}
+				//first check if we are still looking at the protein and not the water box. This can be determined from the last column in the pdb file
+				//WT1 stands for the first water in the molecule which is no longer part of the protein
+				if(atomdata.get(9).equals("WT1"))
+				{
+					//break out of the while loop so set the boolean "loop" to false will break the while
+					loop = false;
+				}
+				//need to loop through each of the atom types to check if the line contains one of the backbone atoms
+				for(int f = 0; f < 4; f++)
+				{
+					//if statement to check for a backbone atom. Atom type is stored in index 2
+					if(atomdata.get(2).equals(bbatoms.get(f)))
+					{
+						//if there is a match we want the line added to the final array so set the atom check variable to true
+						atomcheck = true;
+
+					}
+
+				}
+				//check the atom check variable to see if the line can be added to the final array
+				if(atomcheck == true)
+				{
+					//add the line to the final array. putting it in as an array dereferences the data so the array can be cleared for the next line and no data will be lost
+					linedata.add(atomdata.toArray(new String[10]));
+				}
+				//call the output method to set up the extrabonds file
+				outputToFile(linedata);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+
+
+
+
+	}
+
 
   public static void main(String[] args)
   {
